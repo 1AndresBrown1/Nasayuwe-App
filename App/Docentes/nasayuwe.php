d4bd75b8a7178d4700b33e4b4839f1cce82806e3
SantanderValley <santandervalleycol@gmail.com>
2024-09-28 23:12:04 -0500

    ..

--- App/Docentes/nasayuwe.php
+++ App/Docentes/nasayuwe.php
@@ -68,13 +68,29 @@ if ($result->num_rows > 0) {
 }
 ?>
 
+<div class="p-4">
+    <!-- Sección para mostrar los niveles como pasos (steps) -->
+    <div class="niveles-steps mb-6">
+        <h2 class="text-lg font-bold mb-4">Niveles del Curso</h2>
+        <ul id="course-steps" class="steps">
+            <!-- Aquí se generarán dinámicamente los niveles -->
+        </ul>
+    </div>
 
-<div class="flex">
-    <!-- Sección de reproducción -->
-    <div class="video-section p-4">
+    <!-- Sección para mostrar las lecciones del nivel seleccionado -->
+    <div class="lecciones-list mb-6" id="lesson-list-container" style="display: none;">
+        <h3 class="text-lg font-bold">Lecciones del Nivel</h3>
+        <p>Selecciona una lección</p>
+        <div id="lesson-list" class="mt-4 flex flex-wrap gap-4">
+            <!-- Aquí se generarán dinámicamente las lecciones -->
+        </div>
+    </div>
+
+    <!-- Sección de reproducción y preguntas -->
+    <div class="video-section">
         <div class="mockup-window bg-base-300 border">
-            <div id="media-container">
-                <img id="section-image" src="" alt="Imagen de la lección" class="w-full  object-contain bg-gray-200" style="display: none;">
+            <div  id="media-container">
+                <img id="section-image" src="" alt="Imagen de la lección" class="w-full object-contain bg-gray-200" style="display: none;">
                 <video id="video-player" controls class="video-player" style="display: none;">
                     <source id="video-source" type="video/mp4">
                     Tu navegador no soporta la reproducción de video.
@@ -86,18 +102,25 @@ if ($result->num_rows > 0) {
                 <source id="audio-source" src="" type="audio/mp3">
                 Tu navegador no soporta la reproducción de audio.
             </audio>
-            <p id="lesson-description" class="description rounded-lg p-4 my-5 bg-base-200"></p> <!-- Aquí irá la descripción -->
-            <div id="lesson-question" class="pregunta"></div> <!-- Aquí se mostrarán las preguntas -->
-            <button id="submit-answer" class="btn my-4">Enviar Respuesta</button>
-            <p id="result-message" class="resultado"></p> <!-- Mensaje de resultado -->
-        </div>
-    </div>
 
-    <!-- Sidebar de lecciones -->
-    <div class="course-sidebar ms-2  bg-base-300 shadow-md p-2">
-        <h2 class="text-lg font-bold my-5 mb-4">Contenido del curso</h2>
-        <div id="course-content" class="space-y-4 w-48">
-            <!-- Aquí se generarán dinámicamente las secciones -->
+            <!-- Tabs para preguntas -->
+            <div role="tablist" class="tabs tabs-lifted mt-8">
+                <input type="radio" name="question_tabs" role="tab" class="tab" aria-label="Lección" checked />
+                <div role="tabpanel" class="tab-content bg-base-100 border-base-300 rounded-box p-6">
+                    <h4 class="font-bold">Contenido de la Lección</h4>
+                    <p class="hidden" id="lesson-content"></p>
+                    <p id="lesson-description" class="description rounded-lg p-4 my-5 bg-base-200"></p> 
+
+                </div>
+
+                <input type="radio" name="question_tabs" role="tab" class="tab" aria-label="Pregunta" />
+                <div role="tabpanel" class="tab-content bg-base-100 border-base-300 rounded-box p-6">
+                    <h4 class="font-bold">Pregunta:</h4>
+                    <div id="lesson-question" class="pregunta"></div> <!-- Aquí se mostrarán las preguntas -->
+                    <button id="submit-answer" class="btn my-4">Enviar Respuesta</button>
+                    <p id="result-message" class="resultado"></p> <!-- Mensaje de resultado -->
+                </div>
+            </div>
         </div>
     </div>
 </div>
@@ -107,165 +130,140 @@ if ($result->num_rows > 0) {
     let currentLesson = null;
     let selectedAnswer = null;
 
-    function setCookie(name, value, days) {
-        const d = new Date();
-        d.setTime(d.getTime() + (days * 24 * 60 * 60 * 1000));
-        const expires = "expires=" + d.toUTCString();
-        document.cookie = name + "=" + value + ";" + expires + ";path=/";
-    }
-
-    function getCookie(name) {
-        const decodedCookie = decodeURIComponent(document.cookie);
-        const ca = decodedCookie.split(';');
-        name = name + "=";
-        for (let i = 0; i < ca.length; i++) {
-            let c = ca[i];
-            while (c.charAt(0) == ' ') {
-                c = c.substring(1);
-            }
-            if (c.indexOf(name) == 0) {
-                return c.substring(name.length, c.length);
-            }
-        }
-        return "";
-    }
-
-    function isLessonAnswered(lessonId) {
-        const answeredLessons = getCookie('answeredLessons') || "";
-        return answeredLessons.includes(lessonId);
-    }
-
-    function saveLessonAsAnswered(lessonId) {
-        let answeredLessons = getCookie('answeredLessons') || "";
-        if (!answeredLessons.includes(lessonId)) {
-            answeredLessons += lessonId + ",";
-            setCookie('answeredLessons', answeredLessons, 365);
-        }
-    }
-
     function loadCourseContent() {
-        const courseContent = document.getElementById('course-content');
-        courseContent.innerHTML = ''; // Limpiar el contenido antes de recargarlo
+        const courseSteps = document.getElementById('course-steps');
+        const lessonListContainer = document.getElementById('lesson-list-container');
+        const lessonList = document.getElementById('lesson-list');
 
-        let firstLessonLoaded = false;
+        courseSteps.innerHTML = ''; // Limpiar el contenido antes de recargarlo
 
         Object.values(data).forEach((curso) => {
-            Object.values(curso.niveles).forEach((nivel) => {
-                const nivelDiv = document.createElement('div');
-                nivelDiv.classList.add('mb-4');
+            Object.values(curso.niveles).forEach((nivel, nivelIndex) => {
+                const nivelItem = document.createElement('li');
+                nivelItem.classList.add('step');
+                if (nivelIndex === 0) {
+                    nivelItem.classList.add('step-primary'); // El primer nivel se marca como activo inicialmente
+                }
+                nivelItem.textContent = nivel.nivel_titulo;
 
-                const nivelTitle = document.createElement('h3');
-                nivelTitle.textContent = nivel.nivel_titulo;
-                nivelTitle.classList.add('text-lg', 'font-bold', 'mb-2');
-                nivelDiv.appendChild(nivelTitle);
-
-                Object.values(nivel.lecciones).forEach((leccion, index) => {
-                    const leccionDiv = document.createElement('div');
-                    leccionDiv.classList.add('mb-2', 'border-b', 'pb-2', 'flex', 'items-center');
-
-                         // Agregar el nuevo ícono SVG antes del título de la lección
-                         const svgIcon = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-                    svgIcon.setAttribute("xmlns", "http://www.w3.org/2000/svg");
-                    svgIcon.setAttribute("viewBox", "0 0 24 24");
-                    svgIcon.setAttribute("fill", "currentColor");
-                    svgIcon.classList.add("w-4", "h-4", "mr-2");
-
-                    const svgPath = document.createElementNS("http://www.w3.org/2000/svg", "path");
-                    svgPath.setAttribute("fill-rule", "evenodd");
-                    svgPath.setAttribute("d", "M19.902 4.098a3.75 3.75 0 0 0-5.304 0l-4.5 4.5a3.75 3.75 0 0 0 1.035 6.037.75.75 0 0 1-.646 1.353 5.25 5.25 0 0 1-1.449-8.45l4.5-4.5a5.25 5.25 0 1 1 7.424 7.424l-1.757 1.757a.75.75 0 1 1-1.06-1.06l1.757-1.757a3.75 3.75 0 0 0 0-5.304Zm-7.389 4.267a.75.75 0 0 1 1-.353 5.25 5.25 0 0 1 1.449 8.45l-4.5 4.5a5.25 5.25 0 1 1-7.424-7.424l1.757-1.757a.75.75 0 1 1 1.06 1.06l-1.757 1.757a3.75 3.75 0 1 0 5.304 5.304l4.5-4.5a3.75 3.75 0 0 0-1.035-6.037.75.75 0 0 1-.354-1Z");
-                    svgPath.setAttribute("clip-rule", "evenodd");
-                    svgIcon.appendChild(svgPath);
-
-                    leccionDiv.appendChild(svgIcon);
-                    
-                    const leccionTitle = document.createElement('p');
-                    leccionTitle.classList.add('section-title');
-                    leccionTitle.textContent = `${leccion.leccion_titulo} (${leccion.duracion})`;
-
-                    leccionDiv.appendChild(leccionTitle);
-
-                    leccionTitle.addEventListener('click', () => {
-                        currentLesson = leccion;
-
-                        const image = document.getElementById('section-image');
-                        const video = document.getElementById('video-player');
-                        const audio = document.getElementById('audio-player');
-                        const resultMessage = document.getElementById('result-message');
-
-                        resultMessage.textContent = '';
-
-                        // Mostrar solo el video si hay un video, y el audio si no hay video pero sí imagen
-                        if (leccion.video_url) {
-                            video.src = leccion.video_url;
-                            video.style.display = "block";
-                            image.style.display = "none";
-                            audio.style.display = "none";
-                        } else if (leccion.imagen_url) {
-                            image.src = leccion.imagen_url;
-                            image.style.display = "block";
-                            video.style.display = "none";
-                            audio.src = leccion.audio_url;
-                            audio.style.display = "block";
-                            audio.load();
+                nivelItem.addEventListener('click', () => {
+                    // Marcar todos los pasos anteriores y el actual como activos
+                    const steps = courseSteps.querySelectorAll('.step');
+                    steps.forEach((step, index) => {
+                        if (index <= nivelIndex) {
+                            step.classList.add('step-primary');
                         } else {
-                            image.style.display = "none";
-                            video.style.display = "none";
-                            audio.src = leccion.audio_url;
-                            audio.style.display = "block";
-                            audio.load();
-                        }
-
-                        document.getElementById('lesson-description').textContent = leccion.descripcion;
-
-                        if (isLessonAnswered(leccion.leccion_id)) {
-                            resultMessage.textContent = "Ya has respondido esta lección.";
-                            resultMessage.style.color = "blue";
-                            document.getElementById('submit-answer').disabled = true;
-                            disableRadioButtons();
-                        } else {
-                            document.getElementById('submit-answer').disabled = false;
-                            enableRadioButtons();
-                        }
-
-                        const questionDiv = document.getElementById('lesson-question');
-                        questionDiv.innerHTML = '';
-                        if (leccion.pregunta && leccion.pregunta.texto) {
-                            const questionText = document.createElement('p');
-                            questionText.textContent = leccion.pregunta.texto;
-                            questionDiv.appendChild(questionText);
-
-                            const opcionesDiv = document.createElement('div');
-                            opcionesDiv.classList.add('opciones');
-                            ['opcion1', 'opcion2', 'opcion3', 'opcion4'].forEach((opcionKey) => {
-                                const opcionLabel = document.createElement('label');
-                                const opcionRadio = document.createElement('input');
-                                opcionRadio.type = 'radio';
-                                opcionRadio.name = 'opcion';
-                                opcionRadio.value = opcionKey;
-                                opcionRadio.addEventListener('change', () => {
-                                    selectedAnswer = opcionKey;
-                                });
-
-                                opcionLabel.appendChild(opcionRadio);
-                                opcionLabel.appendChild(document.createTextNode(leccion.pregunta.opciones[opcionKey]));
-                                opcionesDiv.appendChild(opcionLabel);
-                                opcionesDiv.appendChild(document.createElement('br'));
-                            });
-                            questionDiv.appendChild(opcionesDiv);
+                            step.classList.remove('step-primary');
                         }
                     });
 
-                    if (!firstLessonLoaded && index === 0) {
-                        leccionTitle.click();
-                        firstLessonLoaded = true;
-                    }
-
-                    nivelDiv.appendChild(leccionDiv);
+                    // Cargar el nivel y la primera lección automáticamente
+                    loadLevelAndFirstLesson(nivel);
                 });
 
-                courseContent.appendChild(nivelDiv);
+                courseSteps.appendChild(nivelItem);
             });
+
+            // Cargar el primer nivel y la primera lección automáticamente al cargar la página
+            const firstNivel = Object.values(curso.niveles)[0];
+            if (firstNivel) {
+                loadLevelAndFirstLesson(firstNivel);
+            }
         });
+    }
+
+    function loadLevelAndFirstLesson(nivel) {
+        const lessonListContainer = document.getElementById('lesson-list-container');
+        const lessonList = document.getElementById('lesson-list');
+
+        lessonListContainer.style.display = 'block';
+        lessonList.innerHTML = ''; // Limpiar las lecciones antes de añadir las nuevas
+
+        const firstLeccion = Object.values(nivel.lecciones)[0];
+        if (firstLeccion) {
+            loadLesson(firstLeccion);
+        }
+
+        Object.values(nivel.lecciones).forEach((leccion) => {
+            const leccionItem = document.createElement('div');
+            leccionItem.classList.add('badge', 'badge-info', 'gap-2', 'p-4', 'hover:bg-blue-500', 'mb-2');
+            leccionItem.innerHTML = `
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M13.19 8.688a4.5 4.5 0 0 1 1.242 7.244l-4.5 4.5a4.5 4.5 0 0 1-6.364-6.364l1.757-1.757m13.35-.622 1.757-1.757a4.5 4.5 0 0 0-6.364-6.364l-4.5 4.5a4.5 4.5 0 0 0 1.242 7.244" />
+</svg>
+                ${leccion.leccion_titulo}
+            `;
+
+            leccionItem.addEventListener('click', () => {
+                loadLesson(leccion);
+            });
+
+            lessonList.appendChild(leccionItem);
+        });
+    }
+
+    function loadLesson(leccion) {
+        currentLesson = leccion;
+
+        const image = document.getElementById('section-image');
+        const video = document.getElementById('video-player');
+        const audio = document.getElementById('audio-player');
+        const resultMessage = document.getElementById('result-message');
+
+        resultMessage.textContent = '';
+
+        // Mostrar solo el video si hay un video, y el audio si no hay video pero sí imagen
+        if (leccion.video_url) {
+            video.src = leccion.video_url;
+            video.style.display = "block";
+            image.style.display = "none";
+            audio.style.display = "none";
+        } else if (leccion.imagen_url) {
+            image.src = leccion.imagen_url;
+            image.style.display = "block";
+            video.style.display = "none";
+            audio.src = leccion.audio_url;
+            audio.style.display = "block";
+            audio.load();
+        } else {
+            image.style.display = "none";
+            video.style.display = "none";
+            audio.src = leccion.audio_url;
+            audio.style.display = "block";
+            audio.load();
+        }
+
+        document.getElementById('lesson-description').textContent = leccion.descripcion;
+
+        // Mostrar el contenido de la lección
+        document.getElementById('lesson-content').textContent = leccion.descripcion;
+
+        const questionDiv = document.getElementById('lesson-question');
+        questionDiv.innerHTML = '';
+        if (leccion.pregunta && leccion.pregunta.texto) {
+            const questionText = document.createElement('p');
+            questionText.textContent = leccion.pregunta.texto;
+            questionDiv.appendChild(questionText);
+
+            const opcionesDiv = document.createElement('div');
+            opcionesDiv.classList.add('opciones');
+            ['opcion1', 'opcion2', 'opcion3', 'opcion4'].forEach((opcionKey) => {
+                const opcionLabel = document.createElement('label');
+                const opcionRadio = document.createElement('input');
+                opcionRadio.type = 'radio';
+                opcionRadio.name = 'opcion';
+                opcionRadio.value = opcionKey;
+                opcionRadio.addEventListener('change', () => {
+                    selectedAnswer = opcionKey;
+                });
+
+                opcionLabel.appendChild(opcionRadio);
+                opcionLabel.appendChild(document.createTextNode(leccion.pregunta.opciones[opcionKey]));
+                opcionesDiv.appendChild(opcionLabel);
+                opcionesDiv.appendChild(document.createElement('br'));
+            });
+            questionDiv.appendChild(opcionesDiv);
+        }
     }
 
     document.getElementById('submit-answer').addEventListener('click', () => {
@@ -284,7 +282,6 @@ if ($result->num_rows > 0) {
 
             document.getElementById('submit-answer').disabled = true;
             disableRadioButtons();
-            saveLessonAsAnswered(currentLesson.leccion_id);
         } else {
             resultMessage.textContent = "Por favor selecciona una respuesta.";
         }
@@ -297,15 +294,29 @@ if ($result->num_rows > 0) {
         });
     }
 
-    function enableRadioButtons() {
-        const radios = document.querySelectorAll('input[name="opcion"]');
-        radios.forEach((radio) => {
-            radio.disabled = false;
-        });
-    }
-
     loadCourseContent();
 </script>
+
+<div class="fixed bottom-0 bg-base-300 right-0 m-4 w-80 max-w-sm overflow-hidden rounded-lg shadow-md sm:bottom-auto sm:top-0 sm:right-0">
+    <div class="flex">
+        <div class="flex items-center justify-center w-12 bg-blue-500">
+            <svg class="w-6 h-6 text-white fill-current" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
+                <path d="M20 3.33331C10.8 3.33331 3.33337 10.8 3.33337 20C3.33337 29.2 10.8 36.6666 20 36.6666C29.2 36.6666 36.6667 29.2 36.6667 20C36.6667 10.8 29.2 3.33331 20 3.33331ZM21.6667 28.3333H18.3334V25H21.6667V28.3333ZM21.6667 21.6666H18.3334V11.6666H21.6667V21.6666Z" />
+            </svg>
+        </div>
+
+        <div class="px-4 py-2 -mx-3">
+            <div class="mx-3">
+                <span class="font-semibold">Nunca pares de aprender! 
+                </span>
+                <p class="text-lg">
+                    <strong><?php echo $_SESSION['nombre_usuario']; ?></strong>
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
+
 <?php
 include_once __DIR__ . '/../Docentes/footer.php';
 ?>
